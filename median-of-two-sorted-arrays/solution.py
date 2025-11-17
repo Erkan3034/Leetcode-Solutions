d85482d7fcbc9114c464a2b095a3bf7d4e2f8b73
ERKAN TURGUT <159196857+Erkan3034@users.noreply.github.com>
2025-11-17 18:12:40 +0300

    Add solution for median-of-two-sorted-arrays

--- median-of-two-sorted-arrays/solution.py
+++ median-of-two-sorted-arrays/solution.py
@@ -8,14 +8,14 @@ class Solution(object):
         
         merged_array = nums1 + nums2
         
-        sorted_array = sorted(merged_array)
+        sorted_array = sorted(merged_array) # sort array
 
-        n = len(sorted_array)
+        n = len(sorted_array) 
         
         if n == 0:
             return 0.0
 
-        if n % 2 == 1:
+        if n % 2 == 1: # if array len is odd 
             median = float(sorted_array[n // 2])
         else:
             mid1 = sorted_array[(n // 2) - 1]
